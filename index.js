b17cfaf15414630f468653a64162ee943c176f49
dewank07 <dewankrastogi007@gmail.com>
2023-09-25 03:53:48 +0530

    fix: CROS header allowed

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import bodyParser from "body-parser";
 import careerRouter from "./routes/careers.js";
 import env from "dotenv";
 import connectDB from "./db/connect.js";
+import cors from "cors";
 env.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/v1", careerRouter);
